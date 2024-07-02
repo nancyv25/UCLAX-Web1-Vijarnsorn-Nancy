ad5457ea62ba9734bea65aca51f05fa5f2fb647f
Nancy Vijarnsorn <n.vijarnsorn@gmail.com>
2024-07-01 18:35:09 -0700

    L2: Complete

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+import SunAndMoon from "./SunAndMoon/SunAndMoon.jsx";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>Nancy Vijarnsorn</React.StrictMode>
+    <React.StrictMode>
+        <SunAndMoon />
+    </React.StrictMode>
 );
